a69f41ed379734f489c558a3b8863f0f2ed198c4
fooTios <footios76@gmail.com>
2019-01-09 09:22:29 +0200

    Adding-toggle-button

--- src/TryRadium.js
+++ src/TryRadium.js
@@ -1,20 +1,56 @@
 import React from "react";
 import Radium from "radium";
 
-function TryRadium() {
-  const style = {
-    backgroundColor: "green",
-    color: "white",
-    ":hover": {
-      backgroundColor: "red",
-      color: "black"
+class TryRadium extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      MrBool: true
+    };
+  }
+
+  toggleMrBool = () => this.setState({ MrBool: !this.state.MrBool });
+
+  render() {
+    let style = {
+      backgroundColor: "green",
+      color: "white",
+      ":hover": {
+        backgroundColor: "red",
+        color: "black"
+      }
+    };
+    let buttonStyle = {
+      border: "1px solid blue",
+      backgroundColor: "green",
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen"
+      }
+    };
+
+    let paragraph = "";
+    if (this.state.MrBool) {
+      paragraph = <p> Hello again </p>;
+      buttonStyle.backgroundColor = "red";
+      buttonStyle[":hover"] = {
+        backgroundColor: "salmon"
+      };
+    } else {
+      paragraph = null;
     }
-  };
-  return (
-    <div>
-      <p style={style}>Hello</p>
-    </div>
-  );
+    return (
+      <div>
+        <p Key="1" style={style}>
+          Hello
+        </p>
+        <button key="2" style={buttonStyle} onClick={this.toggleMrBool}>
+          toggle hello again
+        </button>
+        {paragraph}
+      </div>
+    );
+  }
 }
 
 export default Radium(TryRadium);
